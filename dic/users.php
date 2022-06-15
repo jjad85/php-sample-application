5225d3dd151080266c1b657b099aabd3a158535c
Juan Jose Arango <jjad85@gmail.com>
2022-06-15 10:39:10 -0500

    Modificacion ruta BaseDatos

--- dic/users.php
+++ dic/users.php
@@ -4,5 +4,5 @@ require __DIR__ .  '/../vendor/autoload.php';
 ini_set('display_errors', 1);
 
 return new Service\UsersService(
-    require "config/db-connection.php"
+    require "../config/db-connection.php"
 );
