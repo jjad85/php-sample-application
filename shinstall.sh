4ef7366765386df30705dedb6416c0cb5668325a
Juan Jose Arango <jjad85@gmail.com>
2022-06-16 15:02:02 -0500

    instalacion website

--- shinstall.sh
+++ shinstall.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-cd ../..
 chmod 775 Makefile
 ./Makefile
 ./composer.phar install
-cp -R html/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp -R docker/website/html/000-default.conf /etc/apache2/sites-available/000-default.conf
 a2ensite 000-default.conf
 a2enmod rewrite
 apache2ctl configtest
